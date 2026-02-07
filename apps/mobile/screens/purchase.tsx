4bba7f9e2a1966fd56743ac4ff1c0e8bb7180a63
tristan-darodes-de-tailly <tristan.darodes-de-tailly@epitech.eu>
2026-02-07 18:41:29 +0100

    feat(purchase-screen): add PurchaseScreen component with styles for payment page

---
+++ apps/mobile/screens/purchase.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+import { styles } from '@/styles/screens/purchase.styles';
+
+export default function PurchaseScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.screen}>
+        <Text style={styles.text}>WIP payment page</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
---
+++ apps/mobile/styles/screens/purchase.styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F7F9',
+  },
+  screen: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '900',
+    color: '#111',
+  },
+});
