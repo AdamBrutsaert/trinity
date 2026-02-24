26e31be74931df7c9e8656c219631cf7bca940fd
Adam Brutsaert <brutsaertadam@yahoo.fr>
2026-02-24 15:18:34 +0100

    chore(api): fixed tests suites names

--- apps/api/src/modules/brands/index.test.ts
+++ apps/api/src/modules/brands/index.test.ts
@@ -59,7 +59,7 @@ async function createCustomerUser(tx: Database) {
 	)._unsafeUnwrap().token;
 }
 
-describe("User module", () => {
+describe("Brands module", () => {
 	let container: StartedPostgreSqlContainer;
 	let connection: ReturnType<typeof createDatabaseConnection>;
 	let api: ReturnType<typeof treaty<ReturnType<typeof createBrandsModule>>>;
